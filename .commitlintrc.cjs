22cbd2e5b579d2193fabe9cfdf1ea62b642f7400
nhanpnt1711 <nhanpnt.dev@gmail.com>
2024-06-06 19:58:36 +0700

    chore: update source code

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -23,6 +23,6 @@ module.exports = {
 		// 'scope-empty': [2, 'never'],
 		'subject-empty': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
-		'header-max-length': [2, 'always', 72],
+		'header-max-length': [2, 'always', 80],
 	},
 };
